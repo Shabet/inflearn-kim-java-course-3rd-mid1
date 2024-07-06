e1be337724434440072f4fc7cd1f3d731ee845dd
Shabet <pakjunyoung@nate.com>
2024-07-06 16:34:00 +0900

    section10 강의 내용 추가

---
+++ src/section10/exception/ex3/exception/NetworkClientExceptionV3.java
@@ -0,0 +1,8 @@
+package section10.exception.ex3.exception;
+
+public class NetworkClientExceptionV3 extends Exception {
+
+    public NetworkClientExceptionV3(String message) {
+        super(message);
+    }
+}
